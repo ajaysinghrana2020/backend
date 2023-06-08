53306abbbbcb7f231bd912da7306b9b68dca8e60
ajay <ajaysinghrana8894@gmil.com>
2023-06-08 14:14:03 +0530

    second

--- src/main/java/com/example/demo/controller/userController.java
+++ src/main/java/com/example/demo/controller/userController.java
@@ -5,16 +5,14 @@ import com.example.demo.model.User;
 import com.example.demo.model.UserRole;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @RestController()
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class userController {
     @Autowired
     private UserService userService;
